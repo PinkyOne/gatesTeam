2c6e9dbfb1549f56262878697495fc3d43c3dd8e
Elistratov Alex <alexasnr56@gmail.com>
2019-02-21 20:46:14 +0530

    Added simple server

--- server/server.py
+++ server/server.py
@@ -1,6 +1,7 @@
 import time
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
+from pathlib import Path
 
 HOST_NAME = 'localhost'
 PORT_NUMBER = 8080
@@ -14,16 +15,38 @@ class MyHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         paths = {
-            '/last_hour': {'body': {'test': 'l1'}},
-            '/today': {'body': {'test': 't'}},
-            '/yesterday': {'body': {'test': 'y'}},
-            '/last_three_days': {'body': {'test': '3'}}
+            '/last_hour': self.get_data('last_hour'),
+            '/today': self.get_data('today'),
+            '/yesterday': self.get_data('yesterday'),
+            '/last_3days': self.get_data('last_3days')
         }
 
         if self.path in paths:
             self.respond(paths[self.path])
         else:
-            self.respond({'body': {'test': 'unknown'}})
+            self.respond({'error': 'Not Found'})
+
+    def get_data(self, type):
+        filename = Path.cwd().joinpath('server_data.json')
+
+        with open(filename) as f:
+            data = json.load(f)
+
+        response = {}
+        try:
+            response["errors"] = data["errors_"+type]
+        except (ValueError, KeyError, TypeError):
+            print("JSON format error")
+
+
+        try:
+            response["data"] = {}
+            for key in list(filter(lambda x: 'today' in x,data["data"][0].keys())):
+                response["data"][key] = data[key]
+        except (ValueError, KeyError, TypeError):
+            print("JSON format error")
+
+        return response
 
     def handle_http(self, body):
         self.send_response(200)
@@ -33,7 +56,7 @@ class MyHandler(BaseHTTPRequestHandler):
         return body
 
     def respond(self, opts):
-        response = self.handle_http(opts['body'])
+        response = self.handle_http(opts)
         self.wfile.write(bytes(json.dumps(response), 'UTF-8'))
 
 if __name__ == '__main__':
